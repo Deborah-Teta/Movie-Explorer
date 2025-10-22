682491e0baada470fb7a2bae9c7116c804afc6a9
Deborah-Teta <154221965+Deborah-Teta@users.noreply.github.com>
2025-10-22 04:56:45 +0200

    category
    users will need to choose basing on a certain category

---
+++ movie-app/src/components/CategoryFilter.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function CategoryFilter({ category, setCategory, movies }) {
+  const categories = [...new Set(movies.map((m) => m.genres).flat())];
+
+  return (
+   <>  
+    <select
+      value={category}
+      onChange={(e) => setCategory(e.target.value)}
+      className="border p-2 rounded mb-4"
+    >
+      <option value="">All Categories</option>
+      {categories.map((cat) => (
+        <option key={cat} value={cat}>{cat}</option>
+      ))}
+    </select>
+   </> 
+  );
+}
+export default CategoryFilter
