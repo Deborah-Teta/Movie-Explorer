94a6138f7502fbc60def6f89d9d5d180b499ef8b
Deborah-Teta <154221965+Deborah-Teta@users.noreply.github.com>
2025-10-23 02:15:10 +0200

    Updates on the Home page
    Added a hero section and styled it, added an background image for the hero section and styled it

---
+++ movie-app/src/components/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import homeImg from '../assets/pics/homeImg.png';
+
+function HeroSection (){
+    return(
+        <section id='home' className='hero relative h-screen md:max-w-2xl overflow-hidden md: flex items-center justify-center mb-5'>
+            <div className='hero-background absolute inset-0 z-0'>
+                <img 
+                src={homeImg}
+                alt='Home background'
+                className='w-full h-full object-cover brightness-70 backdrop-saturate-120'
+                />
+            </div>
+            <div className="hero-content absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  rounded-lg  bg-black bg-opacity-50 text-white p-5 text-center container py-2 rounded-b-full ">
+                <h1 className="text-6xl font-bold mb-5">Watch </h1>
+                <p className="text-2xl mb-5">Relax Every day with movie app.</p>
+            <Link
+                to="/Favorites"
+                className="inline-block px-8 py-3 bg-amber-700 text-white font-semibold rounded-full no-underline hover:bg-amber-900 hover:text-white"
+            >
+                Discover Your Favs
+            </Link>
+      </div>
+        </section>
+    )
+}
+export default HeroSection
--- movie-app/src/pages/Home.jsx
+++ movie-app/src/pages/Home.jsx
@@ -1,43 +1,84 @@
-import React, { useState } from "react";
-import MovieCard from "../components/MovieCard";
-import SearchBar from "../components/SearchBar";
-import CategoryFilter from "../components/CategoryFilter";
-import useFetchMovies from "../hooks/useFetchMovies";
+// import React, { useState } from "react";
+// import MovieCard from "../components/MovieCard";
+// import SearchBar from "../components/SearchBar";
+// import CategoryFilter from "../components/CategoryFilter";
+// import useFetchMovies from "../hooks/useFetchMovies";
 
-function Home({ toggleFavorite, favorites }) {
-  const { movies, loading } = useFetchMovies();
-  const [search, setSearch] = useState("");
-  const [category, setCategory] = useState("All");
 
-  if (loading) return <p className="text-center mt-10">Loading movies...</p>;
+// function Home({ toggleFavorite, favorites }) {
+//   const { movies, loading } = useFetchMovies();
+//   const [search, setSearch] = useState("");
+//   const [category, setCategory] = useState("All");
 
-  const filteredMovies = movies.filter((movie) => {
-    const matchesSearch = movie.name.toLowerCase().includes(search.toLowerCase());
-    const matchesCategory = category === "All" || movie.category === category;
-    return matchesSearch && matchesCategory;
-  });
+//   if (loading) return <p className="text-center mt-10">Loading movies...</p>;
 
-  return (
-    <div className="p-4">
-      <SearchBar search={search} setSearch={setSearch} />
-      <CategoryFilter category={category} setCategory={setCategory} />
+//   const filteredMovies = movies.filter((movie) => {
+//     const matchesSearch = movie.name.toLowerCase().includes(search.toLowerCase());
+//     const matchesCategory = category === "All" || movie.category === category;
+//     return matchesSearch && matchesCategory;
+//   });
 
-      {filteredMovies.length === 0 ? (
-        <p className="text-center text-gray-500 mt-10">No movies found.</p>
-      ) : (
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
-          {filteredMovies.map((movie) => (
-            <MovieCard
-              key={movie.id}
-              movie={movie}
-              toggleFavorite={toggleFavorite}
-              favorites={favorites}
-            />
-          ))}
-        </div>
-      )}
-    </div>
-  );
+//   return (
+//     <div className="p-4">
+//       <SearchBar search={search} setSearch={setSearch} />
+//       <CategoryFilter category={category} setCategory={setCategory} />
+
+//       {filteredMovies.length === 0 ? (
+//         <p className="text-center text-gray-500 mt-10">No movies found.</p>
+//       ) : (
+//         <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-4">
+//           {filteredMovies.map((movie) => (
+//             <MovieCard
+//               key={movie.id}
+//               movie={movie}
+//               toggleFavorite={toggleFavorite}
+//               favorites={favorites}
+//             />
+//           ))}
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default Home;
+import React from 'react'
+import HeroSection from '../components/Hero'
+import SearchBar from '../components/SearchBar'
+
+
+function Home () {
+    return(
+       <>
+         <HeroSection />
+         
+         <section className='py-16 bg-black'>
+            <div className='container mx-auto px-4 md:max-w-2xl'>
+               <SearchBar />
+               <div className='md:flex'>
+                  
+               </div>
+
+            </div>
+      
+
+         </section>
+       </>
+    )
 }
+export default Home
 
-export default Home;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
